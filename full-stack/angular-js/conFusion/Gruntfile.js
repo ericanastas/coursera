0b2c3992fb2c813274ef75a4d86e449155c37938
Eric Anastas <aireq303@gmail.com>
2017-03-05 13:22:31 -0800

    Added copy and clean tasks

--- full-stack/angular-js/conFusion/Gruntfile.js
+++ full-stack/angular-js/conFusion/Gruntfile.js
@@ -1,37 +1,72 @@
 'use strict';
-module.exports = function(grunt){
-    
-    
+module.exports = function (grunt) {
+
+
     //Time how long tasks take. Can help with optimizing build tasks
-        require('time-grunt')(grunt);
-        
+    require('time-grunt')(grunt);
+
     //Automaticaly load required Grunt Tasks
     require('jit-grunt')(grunt);
 
     grunt.initConfig({
-        
-        pkg:grunt.file.readJSON('package.json'),
-        
-        jshint:{
-            options:{
-                jshintrc:'.jshintrc',
-                reporter:require('jshint-stylish')
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
             },
-            
-            all:{
-                src:['Gruntfile.js',
+
+            all: {
+                src: ['Gruntfile.js',
                      'app/scripts/{,*/}*.js'
                     ]
             }
-            
+
+        },
+
+        copy: {
+            dist: {
+                cwd: 'app',
+                src: ['**', '!styles/**/*.css', '!scripts/**/*.js'],
+                dest: 'dist',
+                expand: true
+            },
+
+            fonts: {
+                files: [
+                    {
+                        //for bootstrap fonts
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/bootstrap/dist',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+      }, {
+                        //for font-awesome
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/font-awesome',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+      }
+    ]
+            }
+        },
+
+        clean: {
+            build: {
+                src: ['dist/']
+            }
         }
     });
-    
-    grunt.registerTask('build',['jshint']);
-    grunt.registerTask('default',['build']);
-    
-    
-    
+
+    grunt.registerTask('build', ['jshint']);
+    grunt.registerTask('default', ['build']);
+
+
+
 
 
 };
