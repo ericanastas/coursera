03d47273a2d50942e6b5c2e86268ed2d88e7bab4
Eric Anastas <aireq303@gmail.com>
2017-03-05 13:16:42 -0800

    Updated Grunt config, added .jshintrc

--- full-stack/angular-js/conFusion/Gruntfile.js
+++ full-stack/angular-js/conFusion/Gruntfile.js
@@ -1,9 +1,37 @@
 'use strict';
 module.exports = function(grunt){
+    
+    
+    //Time how long tasks take. Can help with optimizing build tasks
+        require('time-grunt')(grunt);
+        
+    //Automaticaly load required Grunt Tasks
+    require('jit-grunt')(grunt);
 
     grunt.initConfig({
-
+        
+        pkg:grunt.file.readJSON('package.json'),
+        
+        jshint:{
+            options:{
+                jshintrc:'.jshintrc',
+                reporter:require('jshint-stylish')
+            },
+            
+            all:{
+                src:['Gruntfile.js',
+                     'app/scripts/{,*/}*.js'
+                    ]
+            }
+            
+        }
     });
+    
+    grunt.registerTask('build',['jshint']);
+    grunt.registerTask('default',['build']);
+    
+    
+    
 
 
 };
